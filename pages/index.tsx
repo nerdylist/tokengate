61185dd983c5d594ea020e8c06c92c92e4e4a571
Paul <paul@xarg.com>
2023-07-09 14:33:58 -0700

    Minor fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
+// Title: TokenGate
+// Description: A simple token gate for Cardano NFTs
+// Author: NerdyList
+// Version: 0.1.0
+//
+// Resource References:
+// https://github.com/MeshJS/mesh
+// https://docs.blockfrost.io/
+//
+// Repo: https://github.com/nerdylist/tokengate
+//
+// Test Policy: b100d99535d04dc2cd28ffaad90dcc57f01f6dce6b565f4be8fab033
+// Test Token: asset1t84stfqy424gh4nsdsfe5gfqzy4lpe8ssm0fjj
+// Test Project: mainnetvwyJfkUE0zpNS88YopYlHKo0ku3nV2Zc
+
 import { useState, useEffect } from "react";
 import { useWallet, CardanoWallet } from '@meshsdk/react';
 import Cookies from 'js-cookie';
