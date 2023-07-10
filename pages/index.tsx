c888ae53876a5bdca006f2a21b6e386350643f20
Paul <paul@xarg.com>
2023-07-10 05:24:37 -0700

    Updated top comments section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,20 @@
-// Title: TokenGate
-// Description: A simple token gate for Cardano NFTs
-// Author: NerdyList
-// Version: 0.1.6.3
-//
-// Resource References:
-// https://github.com/MeshJS/mesh
-// https://docs.blockfrost.io/
-//
-// Repo: https://github.com/nerdylist/tokengate
-// Main UI: https://raw.githubusercontent.com/nerdylist/tokengate/main/pages/index.tsx
-// Main CSS: https://raw.githubusercontent.com/nerdylist/tokengate/main/styles/globals.css
-//
-// Test Policy: b100d99535d04dc2cd28ffaad90dcc57f01f6dce6b565f4be8fab033
-// Test Token: asset1t84stfqy424gh4nsdsfe5gfqzy4lpe8ssm0fjj
-// Test Project: mainnetvwyJfkUE0zpNS88YopYlHKo0ku3nV2Zc
+/* Title: TokenGate
+  * Description: A simple token gate for Cardano NFTs
+  * Author: NerdyList
+  * Version: 0.1.6.3
+  *
+  * Resource References:
+  * https://github.com/MeshJS/mesh
+  * https://docs.blockfrost.io/
+  *
+  * Repo: https://github.com/nerdylist/tokengate
+  * Main UI: https://raw.githubusercontent.com/nerdylist/tokengate/main/pages/index.tsx
+  * Main CSS: https://raw.githubusercontent.com/nerdylist/tokengate/main/styles/globals.css
+  *
+  * Test Policy: b100d99535d04dc2cd28ffaad90dcc57f01f6dce6b565f4be8fab033
+  * Test Token: asset1t84stfqy424gh4nsdsfe5gfqzy4lpe8ssm0fjj
+  * Test Project: mainnetvwyJfkUE0zpNS88YopYlHKo0ku3nV2Zc
+*/
 
 import { useState, useEffect } from "react";
 import { useWallet, CardanoWallet } from '@meshsdk/react';
