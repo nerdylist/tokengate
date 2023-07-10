88ea8b9db5f09fedce83da76c96f0fda706b8494
Paul <paul@xarg.com>
2023-07-10 05:49:17 -0700

    Updating to 0.1.6.4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 /* Title: TokenGate
   * Description: A simple token gate for Cardano NFTs
   * Author: NerdyList
-  * Version: 0.1.6.3
+  * Version: 0.1.6.4
   *
   * Resource References:
   * https://github.com/MeshJS/mesh
